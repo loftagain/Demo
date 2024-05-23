0996a365391cd766425e652b040b3b2996c29ce8
diana.vigdorova <a@a.com>
2024-05-23 13:47:48 +0300

    Put mapping added

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,5 +28,12 @@ return studentService.getStudents();
     @DeleteMapping(path="{studentId}")
     public void deleteStudent(@PathVariable("studentId") Long studentId){
     studentService.deleteStudent(studentId);
+    }
+    @PutMapping(path="{studentId}") //we use Put when we want to update sth
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email){
+    studentService.updateStudent(studentId, name, email);
     }
 }
